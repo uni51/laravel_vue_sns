1ea1a178ed4ae73b44d59cd552b104890cf4057d
uni51 <uni51@users.noreply.github.com>
2020-06-15 12:23:55 +0900

    7-4 ユーザーが記事をいいね済みかどうかを判定する7-4 ユーザーが記事をいいね済みかどうかを判定する7-4 ユーザーが記事をいいね済みかどうかを判定する7-4 ユーザーが記事をいいね済みかどうかを判定する

--- app/Article.php
+++ app/Article.php
@@ -4,16 +4,42 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Article extends Model
 {
+    /**
+     * @var array
+     */
     protected $fillable = [
         'title',
         'body',
     ];
 
+    /**
+     * @return BelongsTo
+     */
     public function user(): BelongsTo
     {
         return $this->belongsTo('App\User');
+    }
+
+    /**
+     * @return BelongsToMany
+     */
+    public function likes(): BelongsToMany
+    {
+        return $this->belongsToMany('App\User', 'likes')->withTimestamps();
+    }
+
+    /**
+     * @param User|null $user
+     * @return bool
+     */
+    public function isLikedBy(?User $user): bool
+    {
+        return $user
+            ? (bool)$this->likes->where('id', $user->id)->count()
+            : false;
     }
 }
