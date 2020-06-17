8b027902d6d2e188f0acff2b6091b406fb59e2e5
uni51 <uni51@users.noreply.github.com>
2020-06-17 10:51:37 +0900

    8-12 タグごとの記事一覧画面を作成する

--- app/Tag.php
+++ app/Tag.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Tag extends Model
 {
@@ -18,5 +19,13 @@ class Tag extends Model
     public function getHashtagAttribute(): string
     {
         return '#' . $this->name;
+    }
+
+    /**
+     * @return BelongsToMany
+     */
+    public function articles(): BelongsToMany
+    {
+        return $this->belongsToMany('App\Article')->withTimestamps();
     }
 }
