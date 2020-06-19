6aced977bb424693246ade8f972959507d8f6609
uni51 <uni51@users.noreply.github.com>
2020-06-19 11:27:40 +0900

    9-13 フォロー中・フォロワーの一覧を表示する

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -40,6 +40,38 @@ class UserController extends Controller
     }
 
     /**
+     * @param string $name
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function followings(string $name)
+    {
+        $user = User::where('name', $name)->first();
+
+        $followings = $user->followings->sortByDesc('created_at');
+
+        return view('users.followings', [
+            'user' => $user,
+            'followings' => $followings,
+        ]);
+    }
+
+    /**
+     * @param string $name
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function followers(string $name)
+    {
+        $user = User::where('name', $name)->first();
+
+        $followers = $user->followers->sortByDesc('created_at');
+
+        return view('users.followers', [
+            'user' => $user,
+            'followers' => $followers,
+        ]);
+    }
+
+    /**
      * @param Request $request
      * @param string $name
      * @return array|void
