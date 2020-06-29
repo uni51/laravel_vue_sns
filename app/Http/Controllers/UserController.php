1841a6ab3b52bc5ffcab13486bf21c39d662204a
uni51 <uni51@users.noreply.github.com>
2020-06-29 16:23:48 +0900

    11-7 フォロワーの一覧の改善

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -64,7 +64,8 @@ class UserController extends Controller
      */
     public function followers(string $name)
     {
-        $user = User::where('name', $name)->first();
+        $user = User::where('name', $name)->first()
+            ->load('followers.followers');
 
         $followers = $user->followers->sortByDesc('created_at');
 
